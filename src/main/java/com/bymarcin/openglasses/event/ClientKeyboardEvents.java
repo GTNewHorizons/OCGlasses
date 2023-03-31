cd0e79e7f8fcfb86dc9183e9566818db9941e09b
Sampsa <sampo.vanninen@aalto.fi>
2023-03-31 04:03:29 +0300

    Spotless

--- src/main/java/com/bymarcin/openglasses/event/ClientKeyboardEvents.java
+++ src/main/java/com/bymarcin/openglasses/event/ClientKeyboardEvents.java
@@ -1,24 +1,23 @@
 package com.bymarcin.openglasses.event;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MovingObjectPosition;
 
 import org.lwjgl.input.Keyboard;
 
 import com.bymarcin.openglasses.OpenGlasses;
-import com.bymarcin.openglasses.surface.ClientSurface;
 import com.bymarcin.openglasses.network.GlassesNetworkRegistry;
+import com.bymarcin.openglasses.network.packet.GlassesEventPacket;
+import com.bymarcin.openglasses.network.packet.GlassesEventPacket.EventType;
+import com.bymarcin.openglasses.surface.ClientSurface;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
-import com.bymarcin.openglasses.network.packet.GlassesEventPacket;
-import com.bymarcin.openglasses.network.packet.GlassesEventPacket.EventType;
 
 @SideOnly(Side.CLIENT)
 public class ClientKeyboardEvents {
@@ -40,7 +39,16 @@ public class ClientKeyboardEvents {
             MovingObjectPosition pos = ClientSurface.getBlockCoordsLookingAt(p, 5);
             if (pos != null) {
                 GlassesNetworkRegistry.packetHandler.sendToServer(
-                    new GlassesEventPacket(EventType.BLOCK_INTERACT, null, p, pos.blockX, pos.blockY, pos.blockZ, pos.sideHit, '-', -1));
+                        new GlassesEventPacket(
+                                EventType.BLOCK_INTERACT,
+                                null,
+                                p,
+                                pos.blockX,
+                                pos.blockY,
+                                pos.blockZ,
+                                pos.sideHit,
+                                '-',
+                                -1));
             }
             p.openGui(OpenGlasses.instance, 0, p.getEntityWorld(), 0, 0, 0);
             return;
